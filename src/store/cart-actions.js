53185de4b6b764423b878cdfeaa61b36eadf20da
puja.rani <puja.rani@ril.com>
2022-03-07 08:56:40 +0530

    fix: Avoid sending cart data on intial component load

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -46,7 +46,10 @@ export const sendCartData = (cart) => {
         "https://react-http-64178-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
       if (!response.ok) {
